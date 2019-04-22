086cffb101eb67779e670e2c542dfa4a1fb33d3f
jose marte <tumusicalatina@gmail.com>
2019-04-22 10:44:20 -0400

    Updates

--- app.js
+++ app.js
@@ -39,3 +39,44 @@ function loadNews() {
     xhrCall.send()
 }
 */
+
+$(document).ready(function() {
+    $.ajax({
+        url:
+        'https://newsapi.org/v2/top-headlines?' +
+        'country=us&' +
+        'apiKey=66f61007ad934e93a8fbfa06b8da7988',
+        method: "GET",
+        error: function() {
+          console.log("fucked");
+        },
+        success: function(data) {
+            console.log(data.articles);
+            let output = "";
+
+            let latestNews = data.articles;
+            for( let i in latestNews){
+                output += ` <div class="article-ct">
+                <h4>${latestNews[i].title}</h4>
+                <h5>${latestNews[i].author}</h5>
+                <img src="${latestNews[i].urlToImage}" alt="${latestNews[i].title}"/>
+                <p>${latestNews[i].description}</p>
+                <p>${latestNews[i].content}</p>
+                <div class="articleBtn">
+                    <a href="${latestNews[i].url}" target="_blank" >Link</a>
+                </div>
+                <p>Published on: ${latestNews[i].publishedAt}</p>
+            </div>`;
+            }
+            //error message if no data is returned from the API.
+            if (output !== "") {
+                $("#newsResults").html(output);
+            } else {
+                let newsNotFound = "This news isn't available. Please try searching for another topic";
+                $("#newsResults").html(`<div class"noNewsError"> ${newsNotFound} </div>`);
+            }
+        }
+      });
+      
+
+  });
